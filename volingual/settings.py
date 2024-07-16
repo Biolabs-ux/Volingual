3a33223b640c9e46c6ee57e759e7930e8d518e40
Unix IO <84545108+BertinAm@users.noreply.github.com>
2024-07-16 01:15:05 +0000

    updates

--- volingual/settings.py
+++ volingual/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-g-6q_sd_(pkz%*^-$h%i7*ab(h3=dpu49pov4p17=s%*^v!v^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
