9284b9a0a268e919839a6335d2a6f28c0d9f6fce
Unix IO <84545108+BertinAm@users.noreply.github.com>
2024-07-16 08:04:00 +0000

    updates

--- volingual/settings.py
+++ volingual/settings.py
@@ -84,7 +84,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:9000",
-    "https://recip-chat.onrender.com",
+    "https://volingual.onrender.com/",
 
 ]
 
